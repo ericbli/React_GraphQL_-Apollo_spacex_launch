43b9afe7cadb2264f7df4a6e940d8f4df20a9948
EricLi <ericbli.se@gmail.com>
2019-03-11 20:37:20 +0100

    first commit

---
+++ public/precache-manifest.bac4cb0600bba3f7459441e539216b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "364ee436657f89bebe36",
+    "url": "/static/js/main.364ee436.chunk.js"
+  },
+  {
+    "revision": "95b48ea7f84fe400e923",
+    "url": "/static/js/1.95b48ea7.chunk.js"
+  },
+  {
+    "revision": "364ee436657f89bebe36",
+    "url": "/static/css/main.cb4d1dea.chunk.css"
+  },
+  {
+    "revision": "4e349991c2b1beb14ea6302f7fc79c86",
+    "url": "/index.html"
+  }
+];
